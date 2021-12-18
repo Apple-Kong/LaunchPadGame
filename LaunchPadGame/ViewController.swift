04315d163d0b31917689110c24e68464e4a128e2
Apple-Kong <kongthn@gmail.com>
2021-12-19 01:12:22 +0900

    Initial Commit

---
+++ LaunchPadGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LaunchPadGame
+//
+//  Created by GOngTAE on 2021/12/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
